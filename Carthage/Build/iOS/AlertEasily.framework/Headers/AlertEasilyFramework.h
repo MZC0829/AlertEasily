784a79064466681d870d95b38ac480e44907494a
maizhichao <maizhichao5848@dingtalk.com>
2017-11-22 14:46:16 +0800

    add
    add carthage

---
+++ Carthage/Build/iOS/AlertEasily.framework/Headers/AlertEasilyFramework.h
@@ -0,0 +1,19 @@
+//
+//  AlertEasilyFramework.h
+//  AlertEasilyFramework
+//
+//  Created by 麦志超 on 2017/11/22.
+//  Copyright © 2017年 MZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertEasilyFramework.
+FOUNDATION_EXPORT double AlertEasilyFrameworkVersionNumber;
+
+//! Project version string for AlertEasilyFramework.
+FOUNDATION_EXPORT const unsigned char AlertEasilyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertEasilyFramework/PublicHeader.h>
+
+
